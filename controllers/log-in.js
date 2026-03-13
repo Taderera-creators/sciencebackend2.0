1956e0d9959803d91a2ad53d23fcb8b9635136b3
Taderera-creators <tadereratinodaishe@gmail.com>
2026-03-13 12:54:18 +0200

    Descriptive message

--- controllers/log-in.js
+++ controllers/log-in.js
@@ -8,18 +8,21 @@ module.exports = {
   post: async (req, res) => {
     const { email, password } = req.body;
     try {
-      let user = await UserDetails.findOne({
-        email,
+      let user = await UserDetails.findAll({
+        where: {
+          email,
+        },
       });
 
-      if (!user) {
+      console.log(user[0]);
+
+      if (user[0] === undefined) {
         return res.status(400).json({ msg: ERRORS.USER_NOT_FOUND });
       } else {
-        await bcrypt.compare(password, user.password).then((match) => {
+        await bcrypt.compare(password, user[0]?.password).then((match) => {
           if (!match) {
             return res.status(400).json({ msg: ERRORS.USER_NOT_FOUND });
           } else {
-            console.log("done");
             const accessToken = jwt.sign(
               { email },
               "69a6ffae-6664-4dbb-8e5f-cd62ab6c98e6",
